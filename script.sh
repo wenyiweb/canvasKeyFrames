f5c57b6a67140aa8c4424cadd72d91b8bd6a2975
kevin.yang <yangwenyi0@126.com>
2018-03-30 16:31:47 +0800

    add script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="kevin.yang@all-dream.com"
+CORRECT_NAME="wenyiweb"
+CORRECT_EMAIL="yangwenyi0@126.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
