4ae0b2a878fe0f5b6346b0b48edf9218b6ff15b4
kevin.yang <yangwenyi0@126.com>
2018-03-30 17:30:43 +0800

    add test code

---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+require('should');
+const CanvasKeyframes = require('../src/canvas-keyframes');
+
+describe('CanvasKeyframes Test', () => {
+  it('should get "CanvasKeyframes"', () => {
+    console.log(CanvasKeyframes)
+  });
+});
